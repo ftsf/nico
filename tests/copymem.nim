3d08dd07eeffa0a40be1fdbf8eb24f2143c91e97
Jez Kabanov <thesleepless@gmail.com>
2017-05-27 16:43:24 +1000

    fix copymem tests

--- tests/copymem.nim
+++ tests/copymem.nim
@@ -10,20 +10,20 @@ suite "copymem":
     nico.shutdown()
 
   test "copyMemToScreen":
-    var buffer: array[512,uint8]
+    var buffer = newSeq[uint8](512)
     for i in 0..<512:
       buffer[i] = rnd(16).uint8
     for i in 0..10000:
-      copyMemToScreen(rnd(128),rnd(128),rnd(128), buffer[0].addr)
+      copyMemToScreen(rnd(128),rnd(128), buffer)
 
   test "copyMemToScreen negative":
-    var buffer: array[512,uint8]
+    var buffer = newSeq[uint8](512)
     for i in 0..<512:
       buffer[i] = rnd(16).uint8
     for i in 0..10000:
-      copyMemToScreen(rnd(128)-64,rnd(128)-64,rnd(128)-64, buffer[0].addr)
+      copyMemToScreen(rnd(128)-64,rnd(128)-64, buffer)
 
   test "copyPixelsToMem":
-    var buffer: array[512,uint8]
+    var buffer = newSeq[uint8](512)
     for i in 0..10000:
-      copyPixelsToMem(rnd(128),rnd(128),rnd(512), buffer[0].addr)
+      copyPixelsToMem(rnd(128),rnd(128), buffer)
